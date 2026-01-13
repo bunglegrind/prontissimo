bf9b3d95721d25ec0242b5c2c18731b76b6f2338
bunglegrind <bunglegrind@libero.it>
2026-01-13 07:40:56 +0100

    changed test framework

--- test.js
+++ test.js
@@ -1,17 +1,35 @@
 /*jslint node, unordered, fart */
 /*property
-deepEqual, equal, evidence, fallback, fill, length, listeners, log, message,
-myFlag, ok, on, only, parallel, prependListener, race, removeAllListeners,
-removeListener, skip, sequence, throws
+    deepEqual, end, equal, evidence,fill, length, message, myFlag, notEqual, ok,
+    only, parallel, plan, prependListener, removeListener, sequence,
+    teardown, throws
 */
 
-import {before, test} from "node:test";
 import process from "node:process";
-import assert from "node:assert/strict";
-import pronto from "./pronto.js";
-// import parseq from "../parseq/parseq.js"; //for comparison
 
-const empty_callback = (value, reason) => console.log(value, reason);
+import test from "tape";
+import pronto from "./pronto.js";
+
+function hasThrown(event, message, t) {
+    const listener = function (err) {
+        if (
+            is_my_error(err)
+            && (
+                err?.evidence?.message === `${message}`
+                || err?.message === `${message}`
+            )
+        ) {
+            t.ok(true);
+            return t.end();
+        }
+        t.ok(false);
+    };
+    process.prependListener(event, listener);
+
+    return function cancel() {
+        process.removeListener(event, listener);
+    };
+}
 
 function my_error(msg) {
     const err = new Error(msg);
@@ -19,76 +37,48 @@ function my_error(msg) {
 
     return err;
 }
-
-function hasThrown(event, message, done) {
-    const id = setTimeout(function () {
-        return done(new Error("Callback must throw"));
-    }, 1000);
-    const listener = function (err) {
-        if (
-            err?.evidence?.message === `${message}`
-            || err?.message === `${message}`
-        ) {
-            process.removeListener(event, listener);
-            clearTimeout(id);
-            return done();
-        }
-    };
-    process.prependListener(event, listener);
+function is_my_error(err) {
+    return err?.myFlag || err?.evidence?.myFlag;
 }
 
-before(function () {
-    const defaultExceptionListener = process.listeners("uncaughtException")[0];
-    process.removeAllListeners("uncaughtException");
-    process.on("uncaughtException", function (err) {
-        if (!err.myFlag && !err?.evidence?.myFlag) {
-            defaultExceptionListener(err);
-        }
-    });
-
-    const defaultRejectionListener = process.listeners("unhandledRejection")[0];
-    process.removeAllListeners("unhandledRejection");
-    process.on("unhandledRejection", function (err) {
-        if (!err.myFlag && !err?.evidence?.myFlag) {
-            defaultRejectionListener(err);
-        }
-    });
-});
-
-test("empty parallel requestor array", function (ignore, done) {
+test("empty parallel requestor array", function (t) {
+    t.plan(1);
     pronto.parallel([])(function (value, ignore) {
-        assert.deepEqual(value, [], "an empty array is passed as result");
-        return done();
+        t.deepEqual(value, [], "an empty array is passed as result");
+        return;
     }, true);
 });
 
-test("empty sequence requestor array", function (ignore, done) {
+test("empty sequence requestor array", function (t) {
+    t.plan(1);
     pronto.sequence([])(function (value, ignore) {
-        assert.equal(value, true, "value is passed as result");
-        return done();
+        t.equal(value, true, "value is passed as result");
+        return;
     }, true);
 });
 
-test("empty fallback requestor array", function () {
-    assert.throws(
-        () => pronto.fallback([])(empty_callback, true)
-    );
-});
-
-test("empty race requestor array", function () {
-    assert.throws(
-        () => pronto.race([])(empty_callback, true)
-    );
+test("array length must be preserved", function (t) {
+    const len = 5000;
+    pronto.parallel(
+        new Array(len).fill((cb, ignore) => cb(true))
+    )(function (value, reason) {
+        t.notEqual(value, undefined, `no errors: ${reason}`);
+        t.equal(value?.length, len, "same length as input");
+        t.end();
+    }, 1);
 });
 
 test(
     "callback must be invoked just one time for sync requestor",
-    function (ignore, done) {
-        hasThrown(
+    function (t) {
+        const error_message = "Booom!";
+
+        const cancel = hasThrown(
             "uncaughtException",
-            "Booom!",
-            done
+            error_message,
+            t
         );
+        t.teardown(cancel);
 
         let called = 0;
         pronto.sequence([
@@ -97,47 +87,46 @@ test(
             }
         ])(function (value, ignore) {
             called += 1;
-            assert.ok(called < 2, "callback invoked two times");
+            t.ok(called < 2, "callback invoked two times");
+            if (called > 1) {
+                t.end();
+            }
             if (value === undefined) {
                 return;
             }
             if (called === 1) {
-                throw my_error("Booom!");
+                throw my_error(error_message);
             }
         }, 1);
     }
 );
 
-test("must throw", function (ignore, done) {
-    hasThrown(
-        "uncaughtException",
-        "Booom!",
-        done
-    );
-
+test("must throw", function (t) {
     let called = 0;
+    const error_message = "Booom!";
+    const cancel = hasThrown(
+        "uncaughtException",
+        error_message,
+        t
+    );
+    t.teardown(cancel);
+
     pronto.sequence([pronto.sequence([
         function (cb, v) {
             return cb(v);
         }
     ])])(function (value, ignore) {
         called += 1;
-        assert.ok(called < 2);
+        t.ok(called < 2, "callback invoked two times");
+        if (called > 1) {
+            t.end();
+        }
         if (value === undefined) {
             return;
         }
         if (called === 1) {
-            throw my_error("Booom!");
+            throw my_error(error_message);
         }
     }, 1);
-});
 
-test("array length must be preserved", function (ignore, done) {
-    const len = 5000;
-    pronto.parallel(
-        new Array(len).fill((cb, ignore) => cb(true))
-    )(function (value, ignore) {
-        assert.equal(value?.length, len);
-        return done();
-    }, 1);
 });
